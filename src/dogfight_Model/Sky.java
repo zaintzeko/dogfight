3a7a8981f7c356f53ba371745d9b86b399632578
Dorian <Dorian@dorianAsus.mshome.net>
2016-05-24 09:56:00 +0200

    Sky methode vide

---
+++ src/dogfight_Model/Sky.java
@@ -0,0 +1,17 @@
+package dogfight_Model;
+
+import java.awt.Dimension;
+
+public class Sky {
+	public Sky(final Dimension dimension) {
+
+	}
+
+	public Dimension getDimension() {
+
+	}
+
+	public Image getImage() {
+
+	}
+}
